cc182b17997c7b690a8237fee669f17445b823fb
socoolby <socoolby@socoolbydeMacBook-Pro-2.local>
2014-03-21 11:20:27 +0800

    init project

---
+++ app/src/main/java/com/socoolby/unit/Func.java
@@ -0,0 +1,12 @@
+package com.socoolby.unit;
+
+/**
+ * Created by socoolby on 3/21/14.
+ */
+public class Func {
+    public static boolean isDebug=true;
+    public static void Sysout(String info)
+    {
+        System.out.println(info);
+    }
+}
